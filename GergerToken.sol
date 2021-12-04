6da33fdca41f80f4f62b36602564b152f25555c2
corsantic <aydofurkan25@gmail.com>
2021-12-04 23:34:27 +0300

    delete unnecessary

--- GergerToken.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract GLDToken is ERC20 {
-    constructor() ERC20("Gerger", "GGR") {
-        _mint(msg.sender, 1000000 * (10 ** uint256(decimals())));
-    }
-}
